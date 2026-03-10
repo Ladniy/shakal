afd0361ca4d005a56ac8c53bc7c596fde431cb9f
Vladislav Morozov <ladniy101@gmail.com>
2026-03-10 19:10:53 +0300

    add new presets

--- shakal/main.py
+++ shakal/main.py
@@ -9,7 +9,11 @@ def main():
 
     path_to_input_file = qtn.path("Path to input file").ask()
 
-    preset = qtn.select("Preset", choices=["SHAKAL Lite"], use_jk_keys=True).ask()
+    preset = qtn.select(
+        "Preset",
+        choices=["SHAKAL Light", "SHAKAL Medium", "SHAKAL Strong", "SHAKAL ULTRA"],
+        use_jk_keys=True,
+    ).ask()
 
     path_to_output_dir = qtn.path("Path to output file", only_directories=True).ask()
 
@@ -17,19 +21,60 @@ def main():
 
     output_file = os.path.join(path_to_output_dir, output_file_name)
 
-    # example promt ffmpeg -i input.mp4 -vcodec libx264 -crf 28 -preset slow -acodec aac -b:a 128k output.mp4
-    if preset == "SHAKAL Lite":
+    if preset == "SHAKAL Light":
         params = [
             "-vcodec",
-            "libx264",
+            "libx265",
             "-crf",
-            "28",
+            "22",
             "-preset",
             "slow",
+            "-acodec",
+            "copy",
+        ]
+
+    elif preset == "SHAKAL Medium":
+        params = [
+            "-vcodec",
+            "libx265",
+            "-crf",
+            "26",
+            "-preset",
+            "medium",
             "-acodec",
             "aac",
             "-b:a",
             "128k",
+        ]
+
+    elif preset == "SHAKAL Strong":
+        params = [
+            "-vcodec",
+            "libx265",
+            "-crf",
+            "32",
+            "-preset",
+            "fast",
+            "-acodec",
+            "aac",
+            "-b:a",
+            "96k",
+        ]
+
+    elif preset == "SHAKAL ULTRA":
+        params = [
+            "-vf",
+            "scale=320:180,fps=12,scale=1920:1080:flags=neighbor",
+            "-vcodec",
+            "libx265",
+            "-crf",
+            "42",
+            "-preset",
+            "ultrafast",
+            "-acodec",
+            "aac",
+            "-b:a",
+            "48k",
         ]
 
     cmd = ["ffmpeg", "-i", path_to_input_file, *params, output_file]
